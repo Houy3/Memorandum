d86fd0d65d6630e2d98ba3312d3106b9377f5c68
Фаттахов Оскар <fattakhovoe@aquilon-st.ru>
2023-06-02 15:23:42 +0300

    init

---
+++ src/main/java/os/memorandum/MemorandumApplication.java
@@ -0,0 +1,13 @@
+package os.memorandum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorandumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemorandumApplication.class, args);
+    }
+
+}
